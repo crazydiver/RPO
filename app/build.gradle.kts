90b457818a1438bbf0b62b572d2aa99aa354384d
Ivan Gorbachev <iven.gorby@gmail.com>
2025-03-15 23:45:32 +0300

    Java Lab 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,9 +36,14 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     externalNativeBuild {
         cmake {
             path = file("src/main/cpp/CMakeLists.txt")
