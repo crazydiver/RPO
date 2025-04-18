8f5877dd79f7640f967d0397ce7ed8756f76fac7
Ivan Gorbachev <iven.gorby@gmail.com>
2025-04-18 14:15:12 +0300

    Lab 10

--- backend2/front/src/App.js
+++ backend2/front/src/App.js
@@ -1,21 +1,38 @@
 import './App.css';
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-//import {createBrowserHistory} from "history";
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 
 import NavigationBar from "./components/NavigationBar";
 import Home from "./components/Home";
 import LoginComponent from "./components/Login";
+import Login from "./components/Login";
+import Utils from "./utils/Utils";
+import { Provider } from 'react-redux';
+import { store } from './utils/Rdx';
+import { connect } from 'react-redux';
 
-function App() {
+const ProtectedRoute = ({ user, children }) => {
+    return user ? children : <Navigate to={'/login'} />;
+};
+
+function mapStateToProps(state) {
+    const { msg } = state.alert;
+    return { error_message: msg };
+}
+
+const ConnectedProtectedRoute = connect(mapStateToProps)(ProtectedRoute);
+
+function App(props) {
     return (
         <div className="App">
             <BrowserRouter>
                 <NavigationBar />
                 <div className="container-fluid">
+                {props.error_message &&
+                        <div className="alert alert-danger m-1">{props.error_message}</div>}
                     <Routes>
-                        <Route path="home" element={<Home />}/>
-                        <Route path="login" element={<LoginComponent />}/>
+                        <Route path="login" element={<Login />}/>
+                        <Route path="home" element={<ProtectedRoute><Home/></ProtectedRoute>}/>
                     </Routes>
                 </div>
             </BrowserRouter>
@@ -23,4 +40,4 @@ function App() {
     );
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
